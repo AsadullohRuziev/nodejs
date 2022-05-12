c981b90e7ec5ed23a122ff169edf4cbbfb591c92
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-12 10:50:46 +0500

    Step 3: Save the File

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 // Node.js Upload Files
 // The Formidable Module
-// Step 2: Parse the Uploaded File
+// Step 3: Save the File
 
-
-var http = require('http');
 
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      res.write('File uploaded');
-      res.end();
-    });
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
