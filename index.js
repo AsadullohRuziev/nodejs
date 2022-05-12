10c636f866e26958fc97d50d2b82342cb984c736
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-12 09:43:47 +0500

    create file

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 // Node.js File System Module
 
-const http = require('http');
 var fs = require('fs')
 
 
 
-http.createServer(function (req, res) {
-    fs.readFile('demo.html', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    })
-}).listen(8080);
+//create a file named mynewfile1.txt:
+fs.appendFile('mynewfile.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
