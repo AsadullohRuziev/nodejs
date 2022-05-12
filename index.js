b4bda3ad6bf38b60373645a0ec847e7d50b11405
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-12 11:03:17 +0500

    Node.js Send an Email nor work

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-// Node.js Upload Files
-// The Formidable Module
-// Step 3: Save the File
+//npm install nodemailer
 
 
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'asadullohruziev@gmail.com',
+    pass: 'A453816729r#'
   }
-}).listen(8080);
+});
+
+var mailOptions = {
+  from: 'asadullohruziev@gmail.com',
+  to: 'asadbeeeek@mail.ru',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
