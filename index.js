714da210bc05cff84bd5e80ad7ca186d4769e4fa
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-12 09:22:43 +0500

    http module

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// Node.js Core Modules
+//http module 
+
+const http = require('http');
+var url = require('url')
 
 
-var log = require('./log.js');
 
-log.info("Nodejs started")
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month
+  res.end(txt);
+}).listen(8080);
